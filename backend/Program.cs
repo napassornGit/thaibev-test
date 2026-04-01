334a3b688906eaaef9a3098a00852bdc152a2eef
napassornGit <napassornbns.s@gmail.com>
2026-04-01 23:50:57 +0700

    modify: port

--- backend/Program.cs
+++ backend/Program.cs
@@ -82,6 +82,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
@@ -94,5 +100,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+app.Urls.Add("https://localhost:5001");
 
 app.Run();
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7241' // หรือ http://localhost:5125
+  apiUrl: 'https://localhost:5001'
 };
